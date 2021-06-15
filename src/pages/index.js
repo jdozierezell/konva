3c57e4f151a91ea3251ca05ef5dff8e246a20430
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2021-06-15 18:56:24 -0400

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import { Stage, Layer, Rect } from "react-konva"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  const width = 500
+  const height = 200
+  return (
+    <Stage width={width} height={height}>
+      <Layer>
+        <Rect x={0} y={0} width={width} height={height} fill="#0000ff" />
+      </Layer>
+    </Stage>
+  )
 }
